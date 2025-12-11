50acaaeb26bbfda1971070184b95cd8cf250f3f6
StewaDave <Stewa739@crk.umn.edu>
2025-12-11 00:46:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+/* Background Belt Colors and info box ====================================================== */
+
+function whiteBelt() {
+    document.body.className = "";
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="white";
+    document.getElementById("infoBox").style.display = "block";
+    document.getElementById("whiteText").style.display = "block";
+    document.getElementById("blueText").style.display = "none";
+    document.getElementById("purpleText").style.display = "none";
+    document.getElementById("brownText").style.display = "none";
+    document.getElementById("blackText").style.display = "none";
+}
+function blueBelt() {
+    document.body.className = "";
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="blue";
+    document.getElementById("infoBox").style.display = "block";
+    document.getElementById("whiteText").style.display = "none";
+    document.getElementById("blueText").style.display = "block";
+    document.getElementById("purpleText").style.display = "none";
+    document.getElementById("brownText").style.display = "none";
+    document.getElementById("blackText").style.display = "none";
+}
+function purpleBelt() {
+    document.body.className = "";
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="purple";
+    document.getElementById("infoBox").style.display = "block";
+    document.getElementById("whiteText").style.display = "none";
+    document.getElementById("blueText").style.display = "none";
+    document.getElementById("purpleText").style.display = "block";
+    document.getElementById("brownText").style.display = "none";
+    document.getElementById("blackText").style.display = "none";
+}
+function brownBelt() {
+    document.body.className = "";
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="saddlebrown";
+    document.getElementById("infoBox").style.display = "block";
+    document.getElementById("whiteText").style.display = "none";
+    document.getElementById("blueText").style.display = "none";
+    document.getElementById("purpleText").style.display = "none";
+    document.getElementById("brownText").style.display = "block";
+    document.getElementById("blackText").style.display = "none";
+}
+function blackBelt() {
+    document.body.className = "";
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="black";
+    document.getElementById("infoBox").style.display = "block";
+    document.getElementById("whiteText").style.display = "none";
+    document.getElementById("blueText").style.display = "none";
+    document.getElementById("purpleText").style.display = "none";
+    document.getElementById("brownText").style.display = "none";
+    document.getElementById("blackText").style.display = "block";
+}
+
+/* ===================================================================================================================== */
+
